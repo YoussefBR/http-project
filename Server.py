fc563cef5ef5e454fc5a8fe97d88bc12c4be1b88
YoussefBR <yzb5142@psu.edu>
2022-10-16 21:36:49 -0400

    Uploading current project files

---
+++ Server.py
@@ -0,0 +1,10 @@
+from socket import *
+import re 
+
+# Throw in the parser implementation? Allows this to be a bit better of a resume padder. 
+
+# Create the server socket. Diff between socket and http socket?
+
+# Important parser functions:
+#   re.findall()
+#       when iterating to parse for validness use this and check length of the 
