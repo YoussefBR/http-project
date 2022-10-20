2ef7bd6ba0d6c112db746ba6b9ed34c0fdba153b
YoussefBR <yzb5142@psu.edu>
2022-10-20 16:42:40 -0400

    Fully implemented 'GET' requests, but I don't like it and will likely be redesigning the system as I don't feel it's a solid enough foundation to build upon yet. Lots of socket issues that got a bandaid style fix, occasionally takes a while to reconnect client if another TCP request coming from the same port is sent right after the previous one was closed.

--- Client.py
+++ Client.py
@@ -1,17 +1,72 @@
 import socket
+import sys
 
 SERVER_PORT = 6043
 
 CLIENT_PORT_1 = 4000
+SUCCESS_HEADER_SIZE = 15
 HOST = '127.0.0.1'
 
-sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    sock1.bind((HOST, CLIENT_PORT_1))
-except socket.error as error:
-    print('Error ' + error[0] + ': ' + error[1])
-else: 
-    print('Socket 1 successfully created');
+def createReq(request, clientSock):
+        # So we know later if we need to print file names or if we need to create a file to get the requested file
+        if request == "li":
+            li = True
+        else:
+            li = False
+        if request == "exit":
+            return True
 
-sock1.connect((HOST, SERVER_PORT))
+        # Create the request
+        request = "GET " + "http://" + HOST + ":" +  str(SERVER_PORT) + "/" + request + " " + "HTTP/1.1"
+        print("Your request:" + request)
+
+        # Send the request
+        clientSock.send(request.encode())
+
+        # Handle the response
+        response = clientSock.recv(1024)
+        if(response.decode()[0:5] != "Error"):
+            # list handling
+            if(li is True):
+                print(response.decode())
+                return False
+            # file handling
+            else:
+                newFile = open("requestedFile.html", "wb")
+                response = response[SUCCESS_HEADER_SIZE: ]
+                print("Receiving file.")
+                while(response):
+                    newFile.write(response)
+                    response = clientSock.recv(1024)
+                print("File received.")
+                newFile.close()
+        # error handling
+        else:
+            print(response.decode())
+            return False
+        return True
+        
+
+
+clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSock.bind((HOST, CLIENT_PORT_1))
+
+
+try:
+    clientSock.connect((HOST, SERVER_PORT))
+except:
+    print("Connection failed try running Client again")
+    sys.exit()
+print("Successfully connected.")
+
+done = False
+while(done is False):
+    uIn = str(input("Welcome. Please enter the name of the file you'd like. You may also enter 'li' to see a list of all currently available files or 'exit' to end the connection: \n"))
+    done = createReq(uIn, clientSock)
+clientSock.close()
+print("Connection ended.")
+
+
+        
+
 
--- Server.py
+++ Server.py
@@ -1,37 +1,74 @@
 import socket
-import re 
+import re
+import os
+import pdb
 
-# FOR LATER
-# Throw in the parser implementation? Allows this to be a bit better of a resume padder. 
-#   Important parser functions:
-#       re.findall()
-#           when iterating to parse for validness use this and check length of the 
+SUCCESS_HEADER_SIZE = 15
+# Persistant HTTP Server
+class InvalidRequest(Exception):
+    pass
+class LiRequest(Exception):
+    pass
 
-# THIS FILE
-# Create the server socket. Diff between socket and http socket?
+def liConvert():
+    # Update FILE_LIST before sending
+    FILE_LIST = os.listdir("webpages/")
+    # Convert to string to send over TCP
+    string = "Available files:\n"
+    for file in FILE_LIST:
+        string = string + (str(file)) + "\n"
+    return string
 
-class Server:
+def handleRequest(conn):
+    try:
+        req = conn.recv(1024).decode()
+        # To see the request in terminal
+        print("Client sent: " + req)
+        # use re to process the request and get string past "GET " until space or end of in
+        match = re.search(r'http://', req)
+        if(match == None):
+            raise InvalidRequest
+        # Move to filename location in the request
+        start = match.end(0)
+        match = re.search(r"/", req[start:])
+        # Set the start of the file name
+        start = match.end(0) + start
+        match = re.search(r'(\.html)', req)
+        # Check if a list request
+        if(req[start:start+2] == "li"):
+            raise LiRequest
+        # Check if invalid request
+        elif(match is None):
+            raise InvalidRequest
 
-    def __init__(self, database):
-        self.database = database
-    
-    def __enter__(self):
-        pass
+        fileName = req[start:match.end(0)]
+        print(fileName)
+        # try to open file with that name in binary mode
+        reqFile = open("webpages/" + fileName, "rb")
+        conn.send(("HTTP/1.1 200 OK").encode() + reqFile.read(1024 - SUCCESS_HEADER_SIZE))
+        data = reqFile.read(1024)
+        while(data):
+            conn.send(data)
+            data = reqFile.read(1024)
+        return True
+        # on success start sending file, on fail send back error 404
+    except InvalidRequest: 
+        conn.send(("Error: Invalid request, try again\n").encode())
+    except FileNotFoundError:
+        # on file foes not exist this will be returned.
+        conn.send(("Error 404: File not found\n").encode())
+    except LiRequest:
+        conn.send(liConvert().encode())
+    return False
 
-    def __exit__(self):
-
-        pass
 
 HOST = "127.0.0.1"
 PORT = 6043
+FILE_LIST = os.listdir("webpages/")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    sock.bind((HOST, PORT))
-except socket.error as error:
-    print('Error ' + error[0] + ': ' + error[1])
-else: 
-    print('Socket successfully created');
+sock.bind((HOST, PORT))
+print("Server successfully created")
 
 sock.listen() # param can be inserted here to cap the amount of queued connections while server is busy
 
@@ -40,10 +77,20 @@ while True: # Opportunity to beef this up by adding a pass function to create a 
     print('Connection from ' + address[0] + ', port #: ' + str(address[1]))
 
     # Handle http request and return the proper file
+    ret = True
+    closed = False
+    ret = handleRequest(conn)
+    while(ret is False):
+        try:
+            ret = handleRequest(conn)
+        except:
+            ret = True
+            closed = True
 
-    # close connection
-    conn.close()
-
+    # close the conection and move on
+    if(closed is False):
+        conn.shutdown(socket.SHUT_RDWR)
+        conn.close()
     
 
 
